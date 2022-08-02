153fb14244a70a67b08f0381bd80c865a6e794be
Guilherme Castro <guii_1@hotmail.com>
2022-08-01 21:00:42 -0300

    estilização de paginas

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,42 +1,28 @@
 import React from "react";
 import { Navbar } from "react-bootstrap";
 import logoImage from "../../assets/images/logo.png";
-import '../../styles/header.scss'
-import { Link } from 'react-router-dom'
+import "../../styles/header.scss";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <>
-      <Navbar className="py-0">
-        <nav className="navbar navbar-dark bg-danger w-100 ">
-          <div className="d-flex justify-content-between w-100">
-            <Link to="/"> 
-              <img className="ps-5 w-25" src={logoImage} alt="Logo do site"/>
+      <Navbar className="navbar navbar-dark bg-danger wh-100 header-height ">
+          <div className="container">
+            <Link to="/">
+              <img className=".navbar-brand w-25 h-auto" src={logoImage} alt="Logo do site" />
             </Link>
-            <div className="collapse navbar-collapse w-75 d-flex pe-5 justify-content-end" id="navbarNav">
-              <ul className="navbar-nav">
-                <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="home">
-                    Home
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="features">
-                    Features
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="pricing">
-                    Pricing
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link disabled" href="disable">Disabled</a>
-                </li>
-              </ul>
             </div>
-          </div>
-        </nav>
+            <div className="d-flex gap-2 me-5">
+              <Link to="/register">
+                <button type="button" class="btn btn-outline-light">Register </button>
+              </Link>
+              <Link to="/login">
+                <button type="button" class="btn btn-outline-light">
+                  Login
+                </button>
+              </Link>
+            </div>
       </Navbar>
     </>
   );
--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,40 +1,34 @@
 import React from "react";
 import presentationImage from "../../assets/images/presentation.png";
+import { Link } from "react-router-dom";
 import Header from "../../components/header";
-import { Button } from "react-bootstrap";
-import '../../styles/homeScreen.scss'
+import "../../styles/homeScreen.scss";
 
 function HomeScreen() {
   return (
     <>
-      <Header/>
-        <div className="container d-flex align-items-center ">
-          <div className="row g-0">
-            <div className="col-md-8">
-              <div className="card-body">
-                <h5 className="card-title">Create notes easily and access when you wants on the cloud</h5>
-                <p className="card-text">
-                  This is a wider card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
-                </p>
-                <p className="card-text">
-                  <small className="text-muted">Last updated 3 mins ago</small>
-                </p>
-                  <Button className="btn btn-danger">
-                      Register for free now
-                  </Button>
-              </div>
-            </div>
-            <div className="col-md-4">
-              <img
-                src={presentationImage}
-                className="img-fluid rounded-start"
-                alt="Computador apresentando site"
-              />
-            </div>
-          </div>
+      <Header />
+      <div className="container height-container align-items-center justify-content-center row">
+        <div className="ms-5 col-5">
+          <h5 className="card-title fs-2">
+            Create notes easily and access when you wants on the cloud
+          </h5>
+          <p className="card-text   fs-4">
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This content is a little bit longer.
+          </p>
+          <Link to="/register">
+            <button className="btn btn-danger">Register for free now</button>
+          </Link>
         </div>
+      <div className="col-6">
+        <img
+          src={presentationImage}
+          className="ms-5 img-fluid w-100 rounded-start"
+          alt="Computador apresentando site"
+          />
+      </div>
+    </div>
     </>
   );
 }
